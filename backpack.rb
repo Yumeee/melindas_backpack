9d8de5d607099a58faeb65c9af3706b41b51c950
YUMEE <yumeehk@hotmail.com>
2018-07-05 21:45:57 -0400

    test for gym clothes added monday,thursday. Add pack_basic_clothes for pants and shirt.

--- backpack.rb
+++ backpack.rb
@@ -2,34 +2,33 @@ class Backpack
   def initialize(attributes)
     @attributes = attributes # a hash containing day_of_week and weather keys
     @items = []
-    prepare
+    weather_and_day
   end
 
   def items
     @items
   end
 
-  def prepare
-    # set up local variables used by rest of prepare method
-    x = @attributes[:weather]
+  def weather_and_day
+    # set up local variables used by rest of weather_and_day method
+    weather = @attributes[:weather]
     day_of_week = @attributes[:day_of_week]
-
+    pack_basic_clothes
     # Ensure appropriate clothing is added to backpack
-    if x == 'rainy'
-      @items << 'pants'
-      @items << 'shirt'
+    if weather == 'rainy'
       @items << 'umbrella'
-    elsif x == 'cold'
-      @items << 'pants'
-      @items << 'shirt'
+    elsif weather == 'cold'
       @items << 'jacket'
-    else
-      @items << 'pants'
-      @items << 'shirt'
     end
 
+  # Ensure basic clothing is packed
+  def pack_basic_clothes
+    @items << 'pants'
+    @items << 'shirt'
+  end
+
+
     # Ensure gym shoes are added to backpack if it's a gym day
-    # Gotta get to the gym on Tuesdays and Thursdays. Wait a sec...
     if day_of_week == 'monday' || day_of_week == 'thursday'
       #add gym shoes to items
       @items << 'gym shoes'
@@ -39,7 +38,6 @@ class Backpack
     if day_of_week != 'saturday' && day_of_week != 'sunday'
       @items << 'packed lunch'
     elsif false
-      # Used to bring snacks on weekend trips, but now I just buy 'em
       @items << 'snacks'
     end
   end
